c475f84871d3e99f848bb9e5d1efbc9ef4e3d731
Ryo Nakamura <r7kamura@gmail.com>
2015-03-06 20:30:45 +0900

    v0.0.2

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.2
+- Rescue error from Twitter API
+
+## 0.0.1
+- 1st Release :tada:
--- lib/ruboty/twitter_search/version.rb
+++ lib/ruboty/twitter_search/version.rb
@@ -1,5 +1,5 @@
 module Ruboty
   module TwitterSearch
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
