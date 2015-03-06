efe67da58eaee2e37f64c9520c2f32062db0de97
Ryo Nakamura <r7kamura@gmail.com>
2015-03-06 18:53:23 +0900

    bundle gem ruboty-twitter_search

---
+++ lib/ruboty/twitter_search.rb
@@ -0,0 +1,7 @@
+require "ruboty/twitter_search/version"
+
+module Ruboty
+  module TwitterSearch
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/twitter_search/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module TwitterSearch
+    VERSION = "0.0.1"
+  end
+end
